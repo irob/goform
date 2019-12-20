e2308a0c9fc24b5358f117eaf76b05b2f611254b
irob <roberto@formatodigital.com>
2019-12-20 06:07:56 -0600

    code commented

--- goform_example.go
+++ goform_example.go
@@ -1,3 +1,6 @@
+// Package goform Example code
+package goform
+
 // Copyright 2019 by Roberto Morales Olivares. All rights reserved.
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 1.0. If a copy of the MPL was not distributed with this
