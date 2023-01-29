fbd792606ac694998e45fe446d095b44e350c8e9
Roberto MO <roberto@alanmoore.ie>
2023-01-29 12:37:19 +0000

    In development V2

---
+++ README.md
@@ -0,0 +1,164 @@
+goform
+=======
+
+`goform` generate HTML forms dynamically and super easy using Golang/Go.
+
+Status
+=======
+
+In development V2
+
+Description
+=======
+
+`goform` is a super simple form generator, create dynamic forms without having to write HTML.
+
+Templates included:
+- bootstrap5
+- html
+
+By default `goform` render forms in `Bootstrap 5` style, also its posible to choose `html` format, where the inputs are render in plain html (no divs, no labels, ...)
+
+If anyone need a custom template or custom items, `goform` has the option to choose custom template.
+
+TODO:
+- Parse HTML in textlabel element
+- Remove Elements Functions
+- Enable Set/Group of elements
+
+## Example
+
+	package main
+
+	import(
+		"github.com/irob/goform"
+	)
+
+	var res = make(map[string]interface{})
+
+	func main () {
+
+		nInputs := 8
+
+        // CitiesList slice of cities
+        var CitiesList = []OptionItem{{Key: "", Value: "Choose your favorite city"}, {Key: "AMS", Value: "Amsterdam"}, {Key: "VEN", Value: "Venice"}, {Key: "KYO", Value: "Kyoto"}, {Key: "PAR", Value: "Paris"}, {Key: "DOH", Value: "Doha"}, {Key: "BAR", Value: "Barcelona"}, {Key: "SMA", Value: "San Miguel de Allende"}, {Key: "BUD", Value: "Budapest"}, {Key: "LIS", Value: "Lisbon"}, {Key: "FLO", Value: "Florence"}, {Key: "HNK", Value: "Hong Kong"}, {Key: "BRU", Value: "Bruges"}}
+        // AgeRanges slice of ranges of ages
+        var AgeRanges = []OptionItem{{Key: "1", Value: "1 - 9 yo"}, {Key: "2", Value: "10 - 19 yo"}, {Key: "3", Value: "20 - 29 yo"}, {Key: "4", Value: "30 - 39 yo"}, {Key: "5", Value: "40 - 49 yo"}, {Key: "6", Value: ">= 50 yo"}}
+
+        form := Create("profile_form", "POST", "/goform")
+
+        // Label input
+        form.NewElement("label", "userdetails", "User profile")
+
+        // Text input
+        form.NewElement("text", "text", "")
+        form.SetLabel("text", "What's your name")
+
+        // Textlabel input
+        form.NewElement("textlabel", "username", "john@bender.com")
+        form.SetLabel("username", "Your username:")
+
+        // Password input
+        form.NewElement("password", "password", "")
+
+        // Select input
+        form.NewElement("select", "select", "VEN")
+        form.SetOptions("select", CitiesList)
+
+        // Radio input
+        form.NewElement("radio", "radio", "")
+        form.SetLabel("radio", "Age range")
+        form.SetOptions("radio", AgeRanges)
+
+        // Textarea
+        form.NewElement("textarea", "textarea", "")
+        form.SetHelpText("textarea", "Error, must write a resume description")
+
+        // Checkbox
+        form.NewElement("checkbox", "checkbox", "")
+
+        // File input
+        form.NewElement("file", "file", "")
+
+        // Hidden
+        form.NewElement("hidden", "hidden", "")
+
+        // Full address init
+        form.NewElement("label", "address_info", "Full address")
+        form.AddGroupClass("address_info", "col-md-2")
+        form.AddGroupClass("address_info", "mb-2")
+
+        form.NewElement("text", "street", "")
+        form.SetPlaceHolder("street", "Street")
+        form.AddParams("street", "maxlength", "20")
+        form.AddGroupClass("street", "col-md-4")
+        form.AddGroupClass("street", "mb-2")
+
+        form.NewElement("text", "number", "")
+        form.SetPlaceHolder("number", "Number")
+        form.AddParams("number", "maxlength", "20")
+        form.AddGroupClass("number", "col-md-2")
+        form.AddGroupClass("number", "mb-2")
+
+        form.NewElement("select", "city", "VEN")
+        form.SetOptions("city", CitiesList)
+        form.AddGroupClass("city", "col-md-4")
+        form.AddGroupClass("city", "mb-2")
+        // Full address end
+
+        form.NewRow("skills")
+        // Dyanmic inputs
+        for i := 1; i <= nInputs; i++ {
+            form.NewElement("text", "skill_"+strconv.Itoa(i), "")
+            form.SetPlaceHolder("skill_"+strconv.Itoa(i), "Skill "+strconv.Itoa(i))
+            form.AddGroupClass("skill_"+strconv.Itoa(i), "col-md-6")
+            form.AddGroupClass("skill_"+strconv.Itoa(i), "mb-2")
+        }
+
+        // Buttons
+        form.NewElement("submit", "submit", "Update profile")
+        form.AddClass("submit", "btn-success")
+        form.AddClass("submit", "btn-lg")
+        form.AddClass("submit", "btn-xl")
+        form.AddClass("submit", "btn-block")
+
+        form.NewElement("button", "button", "Update profile")
+        form.AddClass("button", "btn-danger")
+        form.AddClass("button", "btn-lg")
+        form.AddClass("button", "btn-xl")
+        form.AddClass("button", "btn-block")
+
+		// Send to template
+		res["Form"] = form
+	}
+
+In your HTML template place
+`{{ .Form.Render }}`
+
+### Requests or bugs?
+<https://github.com/irob/goform/issues>
+
+## Installation
+
+	go get github.com/irob/goform
+
+## Use you custom templates
+
+	/templates/template_for_customize is a templete based on Bootstrap5.
+
+	Step 1.-
+	Move/Copy the folder /templates into your application, rename/copy the subdirectory /template_for_customize.
+
+	Step 2.-
+	Customize the .html files of the elements you want with your own HTML format.
+
+	Step 3.-
+	Change the form style template name to you new template form.SetOwnStyleTemplate(YOUR_CUSTOM_THEME_NAME).
+
+## License
+
+The source files are distributed under the
+[Mozilla Public License, version 2.0](http://mozilla.org/MPL/2.0/),
+unless otherwise noted.
+Please read the [FAQ](http://www.mozilla.org/MPL/2.0/FAQ.html)
+if you have further questions regarding the license.
---
+++ goform.go
@@ -0,0 +1,377 @@
+package main
+
+import (
+	"bytes"
+	"html/template"
+	"log"
+	"os"
+	"path"
+	"sort"
+	"strings"
+)
+
+var (
+	logForm    []ErrorItem
+	fieldTypes = map[string]string{
+		"label":     "label",
+		"text":      "text",
+		"textlabel": "textlabel",
+		"password":  "password",
+		"select":    "select",
+		"radio":     "radio",
+		"textarea":  "textarea",
+		"checkbox":  "checkbox",
+		"file":      "file",
+		"hidden":    "hidden",
+		"button":    "button",
+		"submit":    "submit",
+		"row":       "row",
+	}
+)
+
+// Form structure.
+type Form struct {
+	Name              string
+	ID                string
+	Method            string
+	Action            string
+	MultipartFormData string
+	TemplateStyle     string
+	TemplateSource    string
+	FormTypes         map[string]int
+	Elements          map[string]Field
+	Classes           []string
+	CSS               map[string]string
+	FormText          string
+	FormTemplates     map[string]*template.Template
+	GroupClass        []string
+}
+
+// Element structure.
+type Element struct {
+	ID   int
+	Name string
+}
+
+// Field structure.
+type Field struct {
+	Position    int
+	FieldType   string
+	Name        string
+	ID          string
+	Classes     []string
+	CSS         map[string]string
+	Label       string
+	LabelClass  []string
+	Value       string
+	Options     []OptionItem
+	PlaceHolder string
+	HelpText    string
+	Params      map[string]string
+	Set         string
+	GroupClass  []string
+}
+
+// OptionItem structure.
+type OptionItem struct {
+	Key   string
+	Value string
+}
+
+type SortField struct {
+	Index int
+	Field Field
+}
+
+type ErrorItem struct {
+	FieldName string
+	Message   string
+}
+
+//=============================================================================
+
+// Create configure a new Form structure
+func Create(name string, method string, action string) *Form {
+
+	return &Form{
+		name,
+		name,
+		method,
+		action,
+		"disabled",
+		"bootstrap5",
+		"",
+		make(map[string]int),
+		make(map[string]Field),
+		[]string{},
+		make(map[string]string),
+		"",
+		make(map[string]*template.Template),
+		[]string{},
+	}
+}
+
+// SetMultipartFormData set style format, (html or bootstrap5: default option)
+func (f *Form) SetMultipartFormData(status string) {
+
+	f.MultipartFormData = status
+}
+
+// SetTemplateStyle set style format, (html or bootstrap5: default option)
+func (f *Form) SetTemplateStyle(style string) {
+
+	f.TemplateStyle = style
+}
+
+// SetOwnTemplateStyle set style format, target different templates folder
+// In case if any one need to use a custom templates
+func (f *Form) SetOwnTemplateStyle(style string) {
+
+	f.TemplateStyle = style
+	f.TemplateSource = "OWN"
+}
+
+// DefaultGroupClass set default group classes for all the elements
+func (f *Form) DefaultGroupClass(width string) {
+
+	f.GroupClass = append(f.GroupClass, width)
+}
+
+// Render returns form generated in plain format
+func (f *Form) Render() template.HTML {
+
+	var tmpl *template.Template
+	buf := new(bytes.Buffer)
+	cwd := ""
+	var err error
+
+	if f.TemplateSource == "OWN" {
+		cwd, _ = os.Getwd()
+		cwd += path.Join("/templates/", f.TemplateStyle, ".html")
+
+		tmpl, err = template.ParseFiles(cwd)
+		if err != nil {
+			panic(err)
+		}
+
+	} else {
+		tmpl = HTMLTemplate(f.TemplateStyle, "form")
+	}
+
+	tmpl.Execute(buf, f)
+
+	return template.HTML(buf.String())
+}
+
+// RenderElements returns form elements generated in plain format
+func (f *Form) RenderElements() template.HTML {
+
+	elementsSort := f.SortElements()
+
+	var tmpl *template.Template
+	buf := new(bytes.Buffer)
+	cwd := ""
+	var err error
+
+	// Load ONCE all the necesary templates depending the input types
+	for keyTemplate := range f.FormTypes {
+
+		if f.TemplateSource == "OWN" {
+			cwd, _ = os.Getwd()
+			cwd += path.Join("templates", f.TemplateStyle, keyTemplate, ".html")
+
+			tmpl, err = template.ParseFiles(cwd)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			tmpl = HTMLTemplate(f.TemplateStyle, keyTemplate)
+		}
+
+		f.FormTemplates[keyTemplate] = tmpl
+	}
+
+	// Apply the template to each item of the form
+	for _, itemForm := range elementsSort {
+
+		// Apply the default classes if exists, only if the element have not own group classes
+		if len(itemForm.GroupClass) == 0 && len(f.GroupClass) > 0 {
+			itemForm.GroupClass = f.GroupClass
+		}
+
+		f.FormTemplates[itemForm.FieldType].Execute(buf, itemForm)
+		f.FormText += buf.String()
+
+		// Clear buffer
+		buf = new(bytes.Buffer)
+	}
+
+	return template.HTML(f.FormText)
+}
+
+// EmptyField create and return empty form field
+func EmptyField() Field {
+
+	field := Field{}
+	field.Position = 0
+	field.FieldType = ""
+	field.Name = ""
+	field.ID = ""
+	field.Classes = []string{}
+	field.CSS = map[string]string{}
+	field.Label = ""
+	field.LabelClass = []string{}
+	field.Value = ""
+	field.Options = []OptionItem{}
+	field.PlaceHolder = ""
+	field.HelpText = ""
+	field.Params = map[string]string{}
+	field.GroupClass = []string{}
+
+	return field
+}
+
+// SortElements create and return empty form field
+func (f *Form) SortElements() []Field {
+
+	var elements []SortField
+	for _, v := range f.Elements {
+		elements = append(elements, SortField{v.Position, v})
+	}
+
+	sort.Slice(elements, func(i, j int) bool {
+		return elements[i].Index < elements[j].Index
+	})
+
+	// Elements sorted
+	formElements := []Field{}
+
+	for _, sortField := range elements {
+		formElements = append(formElements, sortField.Field)
+	}
+
+	return formElements
+}
+
+// NewElement insert new form element
+// Returns the last item appended to the slice of elements
+func (f *Form) NewElement(fieldType string, fieldName string, fieldValue string) string {
+
+	// fieldName remove spaces and convert un lowercase
+	fieldName = strings.ToLower(fieldName)
+	fieldName = strings.Replace(fieldName, " ", "", -1)
+
+	_, typeOk := fieldTypes[fieldType]
+	// If the key exists
+	if typeOk {
+
+		field := EmptyField()
+		field.Position = len(f.Elements) + 1
+		field.FieldType = fieldType
+		field.Name = fieldName
+		field.ID = fieldName
+		field.Value = fieldValue
+
+		// Apped/Or Increase the input-type counter of FormTypes map
+		// This map will is used in the RenderElements function
+		f.FormTypes[fieldType]++
+
+		_, fieldOk := f.Elements[fieldName]
+		// If the key exists
+		if !fieldOk {
+			f.Elements[fieldName] = field
+		} else {
+			logForm = append(logForm, ErrorItem{FieldName: fieldName, Message: "Exists"})
+		}
+
+	} else {
+		logForm = append(logForm, ErrorItem{FieldName: fieldType, Message: "Type Do Not Exists"})
+	}
+
+	//log.Println(f.Elements[fieldName])
+
+	return fieldName
+}
+
+// NewRow insert a new row shortcut.
+func (f *Form) NewRow(rowName string) {
+	f.NewElement("row", rowName, "")
+}
+
+// NewButton insert a new button shortcut.
+func (f *Form) NewButton(buttonName string) {
+	f.NewElement("button", buttonName, "")
+}
+
+// SetLabel set/change the text label to the field.
+func (f *Form) SetLabel(fieldName string, label string) {
+	field := f.Elements[fieldName]
+	field.Label = label
+	f.Elements[fieldName] = field
+}
+
+// AddClass adds a class to the input.
+func (f *Form) AddClass(fieldName string, class string) {
+	field := f.Elements[fieldName]
+	field.Classes = append(field.Classes, class)
+	f.Elements[fieldName] = field
+}
+
+// AddCSS add a CSS value (in the form of option-value - e.g.: color - red).
+func (f *Form) AddCSS(fieldName string, key, value string) {
+	f.Elements[fieldName].CSS[key] = value
+}
+
+// AddLabelClass adds a class to the label of the input.
+func (f *Form) AddLabelClass(fieldName string, class string) {
+	field := f.Elements[fieldName]
+	field.LabelClass = append(field.LabelClass, class)
+	f.Elements[fieldName] = field
+}
+
+// SetOptions set/change the Options of the dropdown.
+func (f *Form) SetOptions(fieldName string, options []OptionItem) {
+	field := f.Elements[fieldName]
+	field.Options = options
+	f.Elements[fieldName] = field
+}
+
+// SetPlaceHolder set the placeholder text to the input.
+func (f *Form) SetPlaceHolder(fieldName string, placeholder string) {
+	field := f.Elements[fieldName]
+	field.PlaceHolder = placeholder
+	f.Elements[fieldName] = field
+}
+
+// SetHelpText set the help-text to the input.
+func (f *Form) SetHelpText(fieldName string, helptext string) {
+	field := f.Elements[fieldName]
+	field.HelpText = helptext
+	f.Elements[fieldName] = field
+}
+
+// AddParams add a Param value (in the form of option-value - e.g.: maxlength - 15).
+func (f *Form) AddParams(fieldName string, key, value string) {
+	f.Elements[fieldName].Params[key] = value
+}
+
+// AddGroupClass adds a class to the group input.
+func (f *Form) AddGroupClass(fieldName string, class string) {
+	field := f.Elements[fieldName]
+	field.GroupClass = append(field.GroupClass, class)
+	f.Elements[fieldName] = field
+}
+
+// LogOutput display the Log
+func LogOutput(format string) string {
+	text := ""
+	for _, logField := range logForm {
+		switch format {
+		case "return":
+			text += logField.FieldName + " : " + logField.Message + "\n\r"
+		default:
+			log.Println(logField.FieldName, logField.Message)
+		}
+	}
+	return text
+}
